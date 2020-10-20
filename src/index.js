477d66f365d7aa846905441fe49304f57a902f21
Md. Golam Rabby <32569556+Rabby312@users.noreply.github.com>
2020-10-20 21:08:55 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//This is main page for Daily Schedule
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -5,7 +6,7 @@ import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import { MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol} from "mdbreact";
 import "./index.css";
-
+//catagory select for user
 class App extends Component {
   constructor(props) {
     super(props);
